4bd7884e69fa31c8578b5ac486d775d2a30279d6
HeresJohnny5 <ericksonjohn01@hotmail.com>
2020-03-25 23:56:31 -0400

    Comment out pug template engine. Add express-handlebars template engine and boilerplate files.

--- product/index.js
+++ product/index.js
@@ -1,34 +1,46 @@
 // CORE PACKAGE
-const path = require('path');
+const path = require("path");
 
 // THIRD-PARTY PACKAGES
-const express = require('express');
+const express = require("express");
+const expressHbs = require("express-handlebars");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // CUSTOM ROUTES
-const shopRoutes = require('./routes/shop');
-const adminRoutes = require('./routes/admin');
+const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin");
 
 // STATIC STYLES
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// CONFIGURATION
-app.set('view engine', 'pug');
+// CONFIGURATION FOR PUG
+// app.set("view engine", "pug");
 // The pug template engine ships with built-in express support
-app.set('views', 'views');
+// app.set("views", "views");
+
+// CONFIGURATION FOR EXPRESS-HANDLEBARS
+app.engine(
+  "handlebars",
+  expressHbs({
+    defaultLayout: null,
+    extname: "handlebars"
+  })
+);
+app.set("view engine", "handlebars");
+app.set("views", "views");
 
 // ROUTES
-app.use('/admin', adminRoutes.routes);
+app.use("/admin", adminRoutes.routes);
 // admin is a common starting segment and thus it does not need added to the routes located in the adminRoutes object imported
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404);
-  res.render('404', { docTitle: '404' });
+  res.render("404", { docTitle: "404" });
 });
 
 app.listen(3000);
