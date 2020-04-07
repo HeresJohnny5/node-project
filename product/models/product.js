a9227b1ab6d58304406a208dda2e8ffceef4ffeb
HeresJohnny5 <ericksonjohn01@hotmail.com>
2020-04-07 00:01:12 -0400

    Work on Product controller. Create data directory.

--- product/models/product.js
+++ product/models/product.js
@@ -1,4 +1,6 @@
-const products = [];
+// CORE PACKAGE
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
   constructor(title) {
@@ -6,11 +8,41 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.dirname(
+      process.mainModule.filename, 
+      'data', 
+      'products.json'
+    );
+
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+
+      products.push(this);
+      
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    const p = path.dirname(
+      process.mainModule.filename, 
+      'data', 
+      'products.json'
+    );
+
+    fs.readFile(p, (err, filedContent) => {
+      if (err) {
+        return [];
+      }
+
+      return JSON.parse(filedContent);
+    });
   }
   // The static keyword defines a static method for a class. Static methods aren't called on instances of the class. Instead, they're called on the class itself. These are often utility functions, such as functions to create or clone objects.
 }
